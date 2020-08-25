584310a50142f400cfa21270fd72367878c38891
Ewhan Han <ewhan.han@ibm.com>
2020-08-25 11:40:58 -0400

    Hoisting

---
+++ JS The Complete Guide Section 111-/app.js
@@ -0,0 +1,10 @@
+console.log(userName); //returns undefined!
+var userName = 'Max';
+
+var x = 5;
+var y;
+console.log(x+y);
+y = 7; //Initializations are not hoisted!
+
+console.log(userName_with_let); //Variables defined with let and const are hoisted to the top of the block, but not initialized.
+let userName_with_let = "Ewhan";
