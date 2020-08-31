910fe57699c8d0b09e31acb8c8ef29e322805be4
Ewhan Han <ewhan.han@ibm.com>
2020-08-31 16:28:52 -0400

    Completed section 172

--- JS The Complete Guide Section Section 170-/assets/scripts/app.js
+++ JS The Complete Guide Section Section 170-/assets/scripts/app.js
@@ -1,2 +1,8 @@
-const addMovieModal = document.getElementById("add-modal");//tends to have better performance if getting by ID over querySelector
-const startMovieModal = document.querySelector("header button");// const startMovieModal = document.querySelector("header").lastElementChild;
+const addMovieModal = document.getElementById("add-modal"); //tends to have better performance if getting by ID over querySelector
+const startMovieModal = document.querySelector("header button"); // const startMovieModal = document.querySelector("header").lastElementChild;
+
+const toggleMovieModal = () => {
+  addMovieModal.classList.toggle("visible"); //if visible class is there, remove it. If it's not there add it
+};
+
+startMovieModal.addEventListener("click", toggleMovieModal);
