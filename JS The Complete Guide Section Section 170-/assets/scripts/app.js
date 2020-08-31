7f6203d47ef94edfcbf42f17ce5af1a28c560cc4
Ewhan Han <ewhan.han@ibm.com>
2020-08-31 16:15:47 -0400

    Completed section 171

---
+++ JS The Complete Guide Section Section 170-/assets/scripts/app.js
@@ -0,0 +1,2 @@
+const addMovieModal = document.getElementById("add-modal");//tends to have better performance if getting by ID over querySelector
+const startMovieModal = document.querySelector("header button");// const startMovieModal = document.querySelector("header").lastElementChild;
