c11bf5a9dd5ff7e94a224609c58dbce878fb88fa
Ewhan Han <ewhan.han@ibm.com>
2020-08-31 20:07:34 -0400

    Completed section 173

--- JS The Complete Guide Section Section 170-/assets/scripts/app.js
+++ JS The Complete Guide Section Section 170-/assets/scripts/app.js
@@ -1,8 +1,23 @@
+// "use strict";
+
 const addMovieModal = document.getElementById("add-modal"); //tends to have better performance if getting by ID over querySelector
 const startMovieModal = document.querySelector("header button"); // const startMovieModal = document.querySelector("header").lastElementChild;
+const backdrop = document.getElementById("backdrop");
+const cancelAddMovieButton = document.getElementsByClassName("btn btn--passive")[0];
+
 
 const toggleMovieModal = () => {
   addMovieModal.classList.toggle("visible"); //if visible class is there, remove it. If it's not there add it
+  toggleBackdrop();
 };
 
+const toggleBackdrop = () => {
+  backdrop.classList.toggle("visible");
+};
+
+const cancelAddMovie = (params) => {
+    toggleMovieModal();
+}
+
 startMovieModal.addEventListener("click", toggleMovieModal);
+cancelAddMovieButton.addEventListener("click", cancelAddMovie)
