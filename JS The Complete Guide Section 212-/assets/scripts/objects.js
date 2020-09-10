874b42bf5d7093fa75ef48e06a8fc0558a9c60b0
Ewhan Han <ewhan.han@ibm.com>
2020-09-10 16:55:53 -0400

    Completed section 219

--- JS The Complete Guide Section 212-/assets/scripts/objects.js
+++ JS The Complete Guide Section 212-/assets/scripts/objects.js
@@ -1,3 +1,6 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Object_initializer#Computed_property_names
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Property_accessors
+
 //object literal notation
 //all keys in an object are strings or symbols
 const person = {
@@ -15,7 +18,7 @@ person.greet();
 
 //get property w/ bracket notation, pass in parameter with "" since keys are strings
 console.log(person["String property"]);
-console.log(person["1"]);
+console.log(person[1]); //any other value than a string is 'coerced' to a string
 
 //Monkey patching
 console.log(person.thisVariableDidntexist); //returns undefined, but this becomes a property of the object now
@@ -25,3 +28,22 @@ console.log("We can see that a variable is added during runtime -->", person);
 //delete a property
 delete person.thisVariableDidntexist;
 console.log("Delete this new property using 'delete'", person);
+
+//Computed property names
+const userName = "test";
+const dynamicAccessObj = {
+  [userName]: "test2",
+};
+console.log(
+  "Computed property name is",
+  userName + " with value",
+  dynamicAccessObj[userName]
+); //property name : value --> test: test2
+let i = 0;
+let a = {
+  ["foo" + ++i]: i,
+  ["foo" + ++i]: i,
+  ["foo" + ++i]: i,
+};
+
+console.log("Computed property names and values are:", a.foo1, a.foo2, a.foo3); // 1, 2, 3
