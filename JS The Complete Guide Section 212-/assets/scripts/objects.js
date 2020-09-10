fb262be83de5ba4cd158c4618e870fa647956f82
Ewhan Han <ewhan.han@ibm.com>
2020-09-10 15:42:23 -0400

    Completed section 216

--- JS The Complete Guide Section 212-/assets/scripts/objects.js
+++ JS The Complete Guide Section 212-/assets/scripts/objects.js
@@ -7,5 +7,13 @@ const person = {
     alert("Hello there!");
   },
 };
+// person.greet();//call method
 
-person.greet();
+//Monkey patching
+console.log(person.thisVariableDidntexist); //returns undefined, but this becomes a property of the object now
+person.thisVariableDidntexist = true;
+console.log(person.thisVariableDidntexist);
+console.log("We can see that a variable is added during runtime -->", person);
+//delete a property
+delete person.thisVariableDidntexist;
+console.log("Delete this new property using 'delete'", person);
