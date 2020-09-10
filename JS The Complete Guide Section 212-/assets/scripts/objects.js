667f216f615f577e4d4bca3f98365082f5167770
Ewhan Han <ewhan.han@ibm.com>
2020-09-10 15:36:01 -0400

    Completed section 215

---
+++ JS The Complete Guide Section 212-/assets/scripts/objects.js
@@ -0,0 +1,11 @@
+//object literal notation
+const person = {
+  name: "Ewhan Han",
+  age: 27,
+  hobbies: ["Tennis", "Coding", "Amazon Shopping"],
+  greet: function () {
+    alert("Hello there!");
+  },
+};
+
+person.greet();
