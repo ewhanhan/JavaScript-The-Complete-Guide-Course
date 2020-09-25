41a8fa7d766307d606be5d0d5ec23870ad27c5e5
Ewhan Han <ewhan.han@gmail.com>
2020-09-25 16:00:50 -0400

    Completed section 270

--- JS The Complete Guide Section 265-/app.js
+++ JS The Complete Guide Section 265-/app.js
@@ -1,7 +1,14 @@
-// class Person {
+// class Age {
+//   printAge() {
+//     console.log(this.age);
+//   }
+// }
+
+// class Person extends Age {
 //   name = "Max"; //converted into property when you create the class
 
 //   constructor() {
+//     super();
 //     this.age = 27;
 //   }
 
@@ -19,7 +26,20 @@ function Person() {
       `Hello, my name is ${this.name} and I am ${this.age} years old`
     );
   };
+  // If return is called with an object, then the object is returned instead of this.
+  // If return is called with a primitive, it’s ignored.
+  return "test";
 }
+
+//Replace default __proto__ object with a new one, in the above example the
+//prototype is actually set to Age constructor method
+Person.prototype = {
+  printAge() {
+    console.log(this.age);
+  },
+};
 
 const person = new Person();
 person.greet();
+person.printAge();
+console.log(person.__proto__);
