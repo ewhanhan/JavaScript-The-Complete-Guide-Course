44198299c893d5e0450f5d649baa5c1cf91628c8
ewhanhan <ewhan.han@gmail.com>
2020-09-28 21:00:51 -0400

    Completed section 273

--- JS The Complete Guide Section 265-/app.js
+++ JS The Complete Guide Section 265-/app.js
@@ -13,10 +13,16 @@ class Person extends Age {
   }
 
   greet() {
+    //methods on the parent's prototype are not copied onto __proto__
     console.log(
-      `Hello, my name is ${this.name} and I am ${this.age} years old`
+      `Hello, my name is ${this.name} and I am ${this.age} years old`,
+      this
     );
   }
+
+  // greet = () => {
+  //   console.log(this);
+  // };
 }
 
 class test extends Age {
@@ -28,15 +34,19 @@ class test extends Age {
 
 // function Person() {
 //   this.age = 27;
-//   this.greet = function () {
-//     console.log(
-//       `Hello, my name is ${this.name} and I am ${this.age} years old`
-//     );
-//   };
-//   // If return is called with an object, then the object is returned instead of this.
-//   // If return is called with a primitive, it’s ignored.
-//   return "test";
+//   // this.greet = function () {
+//   //   console.log(
+//   //     `Hello, my name is ${this.name} and I am ${this.age} years old`
+//   //   );
+//   // };
+//   // // If return is called with an object, then the object is returned instead of this.
+//   // // If return is called with a primitive, it’s ignored.
+//   // return "test";
 // }
+
+// Person.prototype.greet = function () {
+//   console.log(`Hello, my name is ${this.name} and I am ${this.age} years old`);
+// };
 
 // Person.describe = function () {
 //   console.log("Creating persons...");
@@ -58,12 +68,46 @@ class test extends Age {
 //   console.log("this is a prototype function");
 // };
 
-// let person = new Person();
+let person = new Person();
 // console.log(person);
-// person.greet();
+person.greet();
 // person.printAge();
 // person.getName();
 // console.log(person.__proto__);
 
 const p = new Person();
+const p2 = new Person();
 console.log(p);
+console.log(p.__proto__ === p2.__proto__); //they use the exact same object (in memory)
+
+// //Method shorthand
+// class Person {
+//   greet() {
+//     console.log("hello");
+//   }
+// }
+
+// //Property function
+// class Person {
+//   greet = function () {
+//     console.log("hello");
+//   };
+//   constructor() {
+//     this.greet2 = function () {
+//       console.log("...");
+//     };
+//   }
+// }
+
+// //Property Arrow function
+// class Person {
+//   greet = () => {
+//     console.log("hello");
+//   };
+
+//   constructor() {
+//     this.greet2 = () => {
+//       console.log("...");
+//     };
+//   }
+// }
